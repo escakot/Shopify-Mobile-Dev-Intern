ad41248c40806536db870865ed62a954e0bfb430
escakot <ec@neorennie.com>
2017-09-05 14:05:58 -0400

    Initial Commit

---
+++ Shopify-Mobile-Dev-Intern/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shopify-Mobile-Dev-Intern
+//
+//  Created by Errol Cheong on 2017-09-05.
+//  Copyright © 2017 Errol Cheong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
